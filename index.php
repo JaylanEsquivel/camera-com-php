fe12a96b0afeea070b475df6053c707f67a6826e
jaylanesquivel <jaylan1997@hotmail.com>
2018-03-24 12:20:15 -0300

    V1.0

--- index.php
+++ index.php
@@ -9,10 +9,9 @@ TECNOLOGIAS DISPONIVEIS: Bootstrap, Jquery, Animate.css, FontAwesome, Normalize.
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/> <!-- ESSA META TIRA ZOOM EM DISPOSITIVOS MOBILES -->
         <meta charset="UTF-8">
-        <title>## PROJETO BASE ##</title>
+        <title>Mobile - Camera PHP</title>
         <link rel="stylesheet" href="css/animate.css" />
         <link rel="stylesheet" href="css/normalize.css" />
         <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css" />
@@ -20,10 +19,18 @@ TECNOLOGIAS DISPONIVEIS: Bootstrap, Jquery, Animate.css, FontAwesome, Normalize.
         <link rel="stylesheet" href="css/style-one.css"/>
     </head>
     <body>
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    
+        <div class="container-fluid bg-geral">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <span class="one">Registro de camera.</span><br>
+                    </div>
+                </div>
+                <div class="row pdd">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <a href="" class="btn btn-success btn-lg two center-block">REGISTRAR AÇÃO</a>&nbsp;&nbsp;
+                        <a href="" class="btn btn-info btn-lg two center-block">GRAVAR AÇÃO</a>
+                    </div>
                 </div>
             </div>
         </div>       
